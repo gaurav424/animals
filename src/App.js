25dd0d3cdaca278b8a58ef85e2942acf2e7d2ffb
gaurav424 <gaurav.rautela424@gmail.com>
2023-01-25 13:28:27 -0500

    Second Commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom/client';
+import { useState } from 'react';
+import AnimalShow from './AnimalShow';
+
+function getRandomAnimal(){
+    const animals = ['bird','cat','cow','dog','gator','horse'];
+
+    return animals[Math.floor(Math.random() * animals.length)];
+}
 
 function App(){
+    const [animals, setAnimals] = useState([]);
 
     const handleClick = () => {
-        console.log('Button was clicked!')
-    }
-    return <div>
-        <button onClick={handleClick}>Add Animal</button>
-    </div>
+        setAnimals([...animals, getRandomAnimal()]);
+    };
+    
+    return (
+        <div>
+            <button onClick={handleClick}>Add Animal</button>
+        </div>
+    );  
 }
 
 export default App;
